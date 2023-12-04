15551491023475624a7cbf1e9b51a60b610a2a8e
Matheus Oliveira <57420634+math-ops@users.noreply.github.com>
2023-12-04 12:49:33 -0400

    Finalizando aulas 3 e 4

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ import app from "./src/app.js";
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log("Agora vc esta no meu dominio");
+  console.log("Ryoiki Tenkai...");
 });
--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import databaseConect from "./config/dbConect.js";
-import livro from "./models/Livro.js";
+import routes from "./routes/index.js";
 
 const conection = await databaseConect();
 
@@ -9,41 +9,10 @@ conection.on("error", (erro) => {
 });
 
 conection.once("open", () => {
-  console.log("conectado com sucesso");
+  console.log("Muryo Kusho");
 });
 
 const app = express();
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.status(200).send("Olha ele ae!");
-});
-
-app.get("/livros", async (req, res) => {
-  const listaLivros = await livro.find({});
-  res.status(200).json(listaLivros);
-});
-
-app.get("/livros/:id", (req, res) => {
-  const index = buscaLivro(req.params.id);
-  res.status(200).json(livros[index]);
-});
-
-app.post("/livros", (req, res) => {
-  livros.push(req.body);
-  res.status(201).send("Livro cadastrado");
-});
-
-app.put("/livros/:id", (req, res) => {
-  const index = buscaLivro(req.params.id);
-  livros[index].title = req.body.title;
-  res.status(200).json(livros);
-});
-
-app.delete("/livros/:id", (req, res) => {
-  const index = buscaLivro(req.params.id);
-  livros.splice(index, 1);
-  res.status(200).send("Livro removido");
-});
+routes(app);
 
 export default app;
